c79dfbcb8563d26e4ae2964601c54b584149cd3c
leflanagan <laurieboo21@gmail.com>
2019-03-20 13:54:01 -0400

    Hello

--- app/src/main/java/com/example/collegecooks/MainActivity.java
+++ app/src/main/java/com/example/collegecooks/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //Hello, this is Joel
 
+    //this is Lauren
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
