3aac71e5d8d0d00f8fe9f3a55186bd114b03dd91
leflanagan <laurieboo21@gmail.com>
2019-03-25 09:20:03 -0400

    Hello

--- app/src/main/java/com/example/collegecooks/DropDown.java
+++ app/src/main/java/com/example/collegecooks/DropDown.java
@@ -3,11 +3,11 @@ package com.example.collegecooks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FirstDownloadScreen extends AppCompatActivity {
+public class DropDown extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_download_screen);
+        setContentView(R.layout.activity_drop_down);
     }
 }
