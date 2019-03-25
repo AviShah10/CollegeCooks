65518139a3b5371084e920797be4fc97c958f740
jwhite20 <joelswhite20@gmail.com>
2019-03-25 09:13:30 -0400

    Added a comment

--- app/src/main/java/com/example/collegecooks/MainActivity.java
+++ app/src/main/java/com/example/collegecooks/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.collegecooks;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 //Hello, this is Joel
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void welcome() {
+    public void welcome(View v) {
         Intent intent = new Intent(this, FirstDownloadScreen.class);
         startActivity(intent);
     }
