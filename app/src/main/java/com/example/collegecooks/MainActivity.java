2a5c70eb6ad87c523cc584c0c0a35b0ef15f5ae3
jwhite20 <joelswhite20@gmail.com>
2019-04-03 08:31:45 -0400

    Added a comment

--- app/src/main/java/com/example/collegecooks/MainActivity.java
+++ app/src/main/java/com/example/collegecooks/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     public void welcome(View v) {
