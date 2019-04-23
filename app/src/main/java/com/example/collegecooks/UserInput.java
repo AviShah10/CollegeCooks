f074aa223dd8e3616957fe0319c542fb4771a300
leflanagan <laurieboo21@gmail.com>
2019-04-23 13:18:39 -0400

    Hello

--- app/src/main/java/com/example/collegecooks/UserInput.java
+++ app/src/main/java/com/example/collegecooks/UserInput.java
@@ -2,9 +2,27 @@ package com.example.collegecooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class UserInput extends AppCompatActivity {
+
+
+    Button submitButton = (Button)(findViewById(R.id.nameInput));
+submitButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+      		....
+		....
+		....
+        }
+    });
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
